82b57b3afa50cbeb63d6217d742f146998d8541f
Hamsterrific <simbul@gmail.com>
2023-01-18 14:13:04 +0400

    fix: parsers lint

--- src/parsers.js
+++ src/parsers.js
@@ -1,15 +1,15 @@
 import yaml from 'js-yaml';
 
-const parse = (file, format) => {
-    if (format === '.json') {
+const parse = (file, extension) => {
+  switch (extension) {
+    case '.yml':
+    case '.yaml':
+      return yaml.load(file);
+    case '.json':
       return JSON.parse(file);
-    }
-    if (format === '.yaml' || format === '.yml') {
-    return yaml.load(file);
-    }
-    else {
+    default:
       throw new Error('Wrong format');
-    }
   }
-  
+};
+
 export default parse;
